5575eb77459b8068f35b9c9300fd29be2dc93f02
Alexande Villegas <villegassanchezjhonalexander@gmail.com>
2019-02-25 02:02:34 -0500

    second

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
 import {MatSelectModule} from '@angular/material';
 import {MatOptionModule} from '@angular/material';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { StatusFilterComponent } from './status-filter/status-filter.component';
 
@@ -22,7 +27,12 @@ import { StatusFilterComponent } from './status-filter/status-filter.component';
     BrowserModule,
     BrowserAnimationsModule,
     MatSelectModule,
-    MatOptionModule
+    MatOptionModule,
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatFormFieldModule,
+    FormsModule
 
   ],
   providers: [],
--- src/app/status-filter/status-filter.component.ts
+++ src/app/status-filter/status-filter.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
 
 
 @Component({
@@ -7,14 +7,43 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './status-filter.component.html',
   styleUrls: ['./status-filter.component.css']
 })
-export class StatusFilterComponent  {
+export class StatusFilterComponent implements OnInit {
+
+    public url:string;
+    public state:string;
 
 
-    foods = [
-      {value: 'steak-0', viewValue: 'Steak'},
-      {value: 'pizza-1', viewValue: 'Pizza'},
-      {value: 'tacos-2', viewValue: 'Tacos'}
+    states = [
+      {value: 'pending-1', viewValue: 'PENDING'},
+      {value: 'processing-2', viewValue: 'PROCESSING'},
+      {value: 'done-3', viewValue: 'DONE'},
+      {value: 'all-4', viewValue: 'ALL'}
     ];
+
+    displayedColumns: string[] = ['ID', 'URL','STATUS' ];
+    dataSource = new MatTableDataSource<urlElements>(ELEMENT_DATA);
+
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+
+    ngOnInit() {
+    this.dataSource.paginator = this.paginator;
+    this.state="all"
+
+    }
 
 
 }
+
+
+export interface urlElements {
+  id: number;
+  Url: string;
+  status: string;
+}
+
+const ELEMENT_DATA: urlElements[] = [
+  {id: 1, Url: 'www.google.com', status: 'PENDING'},
+  {id: 2, Url: 'www.facebook.com', status: 'PROCESSING'},
+  {id: 3, Url: 'www.aviation.ink', status: 'DONE'},
+  {id: 4, Url: 'www.youtube.com', status: 'PENDING'},
+];
